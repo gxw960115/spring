79f16e588404a763bbe7842efa11f319942a5f4c
gxw960115 <gxw960115@outlook.com>
2022-10-12 21:29:13 +0800

    spring学习代码初始化

---
+++ itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -0,0 +1,22 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl创建.....");
+    }
+
+    public void init(){
+        System.out.println("初始化方法......");
+    }
+
+    public void destory(){
+        System.out.println("销毁方法........");
+    }
+    @Override
+    public void save() {
+        System.out.println("save running......");
+    }
+}
---
+++ itheima_spring_ioc/src/test/java/com/itheima/test/SpringTest.java
@@ -0,0 +1,21 @@
+package com.itheima.test;
+
+import com.itheima.dao.UserDao;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+
+    @Test
+    // 测试scope属性
+    public void test1() {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao1 = (UserDao) app.getBean("userDao");
+        UserDao userDao2 = (UserDao) app.getBean("userDao");
+        userDao1.save();
+        userDao2.save();
+        System.out.println(userDao1);
+        System.out.println(userDao2);
+    }
+}
